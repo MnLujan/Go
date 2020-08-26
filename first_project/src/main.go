9ba7ce6b6854c97a7b2827cc31c674e992d04039
Martin Lujan <martinlujan96@gmail.com>
2020-08-26 15:21:39 -0300

    update gitignore

--- first_project/src/main.go
+++ first_project/src/main.go
@@ -6,14 +6,4 @@ import (
 func main() {
 	fmt.Println("Hola perri")
 
-	/* Variables */
-	//"var" - nombre - Tipo de dato
-	var X int
-
-	//Tambien puedo definir varias a la vez
-	var y,z,u int
-
-	///De ser bool el valor, este comienza riempre en modo "false"
-
-
 }
