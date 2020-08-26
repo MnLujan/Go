a994c6d3103a56098d8248f7eb8218a606ac2a76
Martin Lujan <martinlujan96@gmail.com>
2020-08-26 20:06:35 -0300

    update CurseGo

--- first_project/src/main.go
+++ first_project/src/main.go
@@ -1,9 +1,29 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hola perri")
-
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(nil)
+	} else {
+		fmt.Println(text)
+	}
+	arreglo := [5]int{1,2,3,4}
+	fmt.Println(arreglo)
+	aux := arreglo[0:2]
+	fmt.Println(aux)
+	fmt.Println(arreglo)
+	aux = append(aux,5)
+	fmt.Println(aux)
+	aux2 := make([]int,3,6)
+	fmt.Println(aux2)
+	aux2 = append(aux2, 8)
+	fmt.Println(aux2)
 }
